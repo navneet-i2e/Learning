2eae136a368dc57eae856c366f8d766ebf9f464f
navneet-i2e <navneet.anand@i2econsulting.com>
2024-12-23 14:52:23 +0530

    sample sync and async code of js

--- C#/obj/Debug/net8.0/C#.AssemblyInfo.cs
+++ C#/obj/Debug/net8.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6f859b5c98c24d36f2ecac8a13d9f1dde02cb2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f711b7d1535ae3c1af3c97155e488e783952876")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
